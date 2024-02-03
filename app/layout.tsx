6fd8d1e19e125f1a700ff395480b63c0e25bcb4d
marcAvn <ivan2006087@gmail.com>
2024-02-03 17:29:39 +0200

    correcting a syntax error

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ import AuthContext from './context/AuthContext'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'OneMessenger',
-  description: 'OneMessenger',
+  title: 'One Messenger',
+  description: 'One Messenger',
 }
 
 export default function RootLayout({
--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -11,10 +11,10 @@ export default async function UsersLayout({
     
     return (
         <Sidebar>
-        <div className="h-full">
-            <UserList items={users} />
-            {children}
-        </div>
+            <div className="h-full">
+                <UserList items={users} />
+                {children}
+            </div>
         </Sidebar>
     )
 };
