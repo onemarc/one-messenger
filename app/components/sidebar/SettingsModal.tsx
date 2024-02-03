4899cdb847c20d24667299b60d78376270bd5ec6
marcAvn <ivan2006087@gmail.com>
2024-02-03 20:35:29 +0200

    save and cancel buttons in the user data editing window

--- app/components/sidebar/SettingsModal.tsx
+++ app/components/sidebar/SettingsModal.tsx
@@ -115,6 +115,23 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
                             </div>
                         </div>
                     </div>
+
+                    <div className="mt-6 flex items-center justify-end gap-x-6">
+                        <Button
+                            disabled={isLoading}
+                            secondary
+                            onClick={onClose}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            disabled={isLoading}
+                            type="submit"
+                        >
+                            Save
+                        </Button>
+                    </div>
+
                 </div>
             </form>
         </Modal>
