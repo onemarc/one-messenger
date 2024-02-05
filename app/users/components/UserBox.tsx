2b03278e56cef669b4acc2c90ec8df0c405e085a
marcAvn <ivan2006087@gmail.com>
2024-02-05 16:32:43 +0200

    loading modal for the users page

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import axios from "axios";
+import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
+
 import { User } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
-import Avatar from "@/app/components/Avatar";
 
 interface UserBoxProps {
     data: User
@@ -29,22 +31,25 @@ const UserBox: React.FC<UserBoxProps> = ({
     }, [data, router]);
 
     return (
-        <div onClick={handleClick}
-            className="w-full relative flex items-center space-x-3 bg-white
-                p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
-        >
-            <Avatar user={data} />
-            <div className="min-w-0 flex-1">
-                <div className="focus:outline-none">
-                    <div className="flex justify-between items-center mb-1">
-                        <p className="text-sm font-medium text-gray-900">
-                            {data.name}
-                        </p>
+        <>
+            <LoadingModal />
+            <div onClick={handleClick}
+                className="w-full relative flex items-center space-x-3 bg-white
+                    p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
+            >
+                <Avatar user={data} />
+                <div className="min-w-0 flex-1">
+                    <div className="focus:outline-none">
+                        <div className="flex justify-between items-center mb-1">
+                            <p className="text-sm font-medium text-gray-900">
+                                {data.name}
+                            </p>
+                        </div>
                     </div>
-                </div>
 
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
